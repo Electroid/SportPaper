0786131d72b17c7fd741030cd7ff084cdad152bc
chatasma <nodeadbatteries@gmail.com>
2021-01-22 22:28:41 -0600

    Fix JAR copy

--- scripts/basepaperclip.sh
+++ scripts/basepaperclip.sh
@@ -9,7 +9,7 @@ cp ./work/1.8.8/1.8.8.jar ./Paperclip/minecraft_server.1.8.8.jar
 cd ./Paperclip
 mvn -Dmcver=1.8.8 -Dvanillajar=../minecraft_server.1.8.8.jar -Dpaperjar=../sportpaper-1.8.8.jar clean package
 cd ..
-cp ./Paperclip/target/paperclip*-SNAPSHOT.jar ./../../sportpaper-paperclip.jar
+cp ./Paperclip/assembly/target/paperclip-1.8.8.jar ./sportpaper-paperclip.jar
 
 echo ""
 echo ""
